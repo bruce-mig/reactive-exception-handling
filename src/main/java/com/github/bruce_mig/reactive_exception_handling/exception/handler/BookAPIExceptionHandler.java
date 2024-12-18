dc437da9753e41c692238f382fc50507d33ce035
bruce-mig <brucetmig@icloud.com>
2024-12-18 20:52:21 +0200

    custom global exception handling

---
+++ src/main/java/com/github/bruce_mig/reactive_exception_handling/exception/handler/BookAPIExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.github.bruce_mig.reactive_exception_handling.exception.handler;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@Component
+public class BookAPIExceptionHandler extends AbstractErrorWebExceptionHandler {
+
+    /**
+     * Create a new {@code AbstractErrorWebExceptionHandler}.
+     *
+     * @param errorAttributes    the error attributes
+     * @param resources          the resources configuration properties
+     * @param applicationContext the application context
+     * @since 2.4.0
+     */
+    public BookAPIExceptionHandler(ErrorAttributes errorAttributes, WebProperties.Resources resources,
+                                   ApplicationContext applicationContext, ServerCodecConfigurer configurer) {
+        super(errorAttributes, resources, applicationContext);
+        this.setMessageReaders(configurer.getReaders());
+        this.setMessageWriters(configurer.getWriters());
+    }
+
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
+    }
+
+    private Mono<ServerResponse> renderErrorResponse(ServerRequest request) {
+        Map<String, Object> errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        errorAttributes.remove("status");
+        errorAttributes.remove("requestId");
+        return ServerResponse.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(errorAttributes);
+        // todo: set http status dynamically
+    }
+}
